16c1620cf988611b47ace2d1dff6d7e6ea9bfe0b
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-05-10 18:51:53 +0530

    cart page modification

--- app/(roots)/cart/page.tsx
+++ app/(roots)/cart/page.tsx
@@ -1,13 +1,15 @@
 import Container from "@/app/customComponents/container"
 import CartProducts from "./cartProducts"
-
+import React, { Suspense } from 'react';
 
 const CartPage=()=>{
     
     return(
         <Container>
             <div className="font-bold text-2xl my-4">Your Carts Products</div>
-            <CartProducts />
+            <Suspense fallback={<div>Loading...</div>}>
+                <CartProducts />
+            </Suspense>
         </Container>
     )
 }
