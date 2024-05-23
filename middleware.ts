b8655a142fde6509a6321e12f50f5a382e21dcf6
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-05-23 20:04:23 +0530

    clerk version change

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,34 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+// import {
+//   clerkMiddleware,
+//   createRouteMatcher
+// } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/favourites(.*)',
-  '/order(.*)',
-  '/cart(.*)',
-  // '/'
-]);
+// const isProtectedRoute = createRouteMatcher([
+//   '/favourites(.*)',
+//   '/order(.*)',
+//   '/cart(.*)',
+  
+// ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-},{debug:true})
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// },{debug:true})
 
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
+export default authMiddleware({
+  // Allow signed out users to access the specified routes:
+  publicRoutes: ["/api/:path*"],
+});
+ 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-
 
