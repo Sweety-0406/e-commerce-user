5b3e8d403c9e19a1032770c52a56eda928f0d8d5
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-06-01 16:13:01 +0530

    add return

--- app/(roots)/layout.tsx
+++ app/(roots)/layout.tsx
@@ -13,7 +13,7 @@ export default async function DashboardLayout({
 }) {
     const {userId} = auth()
     if(!userId){
-        redirect('/sign-in')
+       return redirect('/sign-in')
     }
 
     return(
